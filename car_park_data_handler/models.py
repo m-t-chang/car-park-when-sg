edcff7729725f8ddab59b018dec6b6b541535529
mtchang <michaeltgchang@gmail.com>
2022-02-02 15:54:31 +0800

    set up django project; initial models
    NOTE: no migrations, did not test anything

---
+++ car_park_data_handler/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Carpark(models.Model):
+    car_park_id = models.CharField(max_length=10, primary_key=True)
+    area = models.CharField(max_length=50)
+    development = models.CharField(max_length=50)
+    location_lat = models.FloatField()
+    location_lon = models.FloatField()
+    lot_type = models.CharField(max_length=10)
+    agency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.car_park_id
+
+
+class Measurement(models.Model):
+    car_park_id = models.ForeignKey(Carpark, on_delete=models.DO_NOTHING, primary_key=True)
+    timestamp = models.DateTimeField(primary_key=True)
+    available_lots = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.car_park_id + " at " + self.timestamp
