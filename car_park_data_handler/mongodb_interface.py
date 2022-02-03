9b55dcae4347fc87807fd639c6ce03d2a9e888f9
mtchang <michaeltgchang@gmail.com>
2022-02-03 15:02:32 +0800

    exploratory data analysis, testing out D3

--- car_park_data_handler/mongodb_interface.py
+++ car_park_data_handler/mongodb_interface.py
@@ -31,3 +31,41 @@ def get_car_park_details(car_park_id):
         list_of_data.append(data_tuple)
 
     return list_of_data
+
+
+def create_aggregated_data():
+    """ create aggregated data, in a format for visualization"""
+
+    # connect to MongoDB
+    client = MongoClient(MONGODB_URI)
+    db = client.car_park_when_sg
+
+    # init data
+    # okay, the raw data is a 3D list. First index is day of week 0-6, second index is hour of day 0-23, and final index is measurement ID.
+    # aggregated data is a 2D list.
+    # the key is the CarParkID-LotType
+    # test_data = [{   '12345-C': [[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15],[12, 14, 15]
+    #                       [1, 12, 14],
+    #                       [1, 12, 14],
+    #                       [1, 12, 14],
+    #                       [1, 12, 14],
+    #                       [1, 12, 14],
+    #                       [1, 12, 14]
+    #                       ]},
+    #             'data_aggregated':
+    #             ]
+
+    data_dict = {}
+    for doc in db.api_responses.find():
+        pass
+
+# for each doc (api snapshot)
+## extract the timestamp index - day of week and hour
+## for each carpark
+### append the availability into the right array
+### move onto next
+
+
+# pprint(doc['timestamp'])
+# dt_object = datetime.fromtimestamp(doc['timestamp'])
+# pprint(dt_object)
--- scripts/read_mongodb.py
+++ scripts/read_mongodb.py
@@ -33,23 +33,41 @@ if __name__ == '__main__':
     set_development = set()
     set_lot_type = set()
     set_car_park_id = set()
+    set_key = set()  # test of my compound key
+    list_car_park_id = []
+    count_lots = 0
 
     # from one doc, list all the Agencies
     for lot in result["data"]:
+        count_lots += 1
         set_agency.add(lot["Agency"])
         set_area.add(lot["Area"])
         set_development.add(lot["Development"])
         set_lot_type.add(lot["LotType"])
         # print(f'{lot["Agency"]}: {lot["CarParkID"]}')
         set_car_park_id.add(lot["CarParkID"])
+        set_key.add(f'{lot["CarParkID"]}-{lot["LotType"]}')
+        list_car_park_id.append(lot["CarParkID"])
     pprint(set_agency)
     pprint(set_area)
     # pprint(set_development)
     pprint(set_lot_type)
     pprint(set_car_park_id)
+    print(f'number of lots: {count_lots}')
+    print(f'number of unique IDs: {len(set_car_park_id)}')
+    print(f'number of unique IDs I made: {len(set_key)}')
+    print(f'number of IDs: {len(list_car_park_id)}')
+    # list_car_park_id.sort()
+    # pprint(list_car_park_id)
 
     print(len(set_development))
     print(len(result["data"]))
+
+    # why duplicates
+    # CARPARKID is duplicated because LotType may be different
+    for lot in result["data"]:
+        if lot["CarParkID"] == 'T0103':
+            pprint(lot)
 
     ###################### GET TREND ####################
     id_to_show = 'L0104'
