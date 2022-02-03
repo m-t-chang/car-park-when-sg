28351731399d859de5405758733cccb4ad619b6a
mtchang <michaeltgchang@gmail.com>
2022-02-03 12:24:42 +0800

    basic django template

--- car_park_data_handler/views.py
+++ car_park_data_handler/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     # members = Members.objects.all()[1:20]  # example of slicing
     # output = ', '.join([member.name for member in members])
     # return HttpResponse(output)
-    return HttpResponse('hello world')
+    context = {"message": "Hello World! This is templating!"}
+    return render(request, 'car_park_data_handler/index.html', context)
