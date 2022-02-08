2d36f458412369aadf1663063c43c6147e5eee89
mtchang <michaeltgchang@gmail.com>
2022-02-08 16:48:05 +0800

    carpark detail (give an array of available lots)

--- restful_apis_with_jwt/urls.py
+++ restful_apis_with_jwt/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # user management
+    path('user/signup/', views.UserAddNew.as_view(), name='user_add_new'),
+
     # data
     path('carpark-list/', views.CarparkList.as_view()),
-
-    # user management
-    path('user/signup/', views.UserAddNew.as_view(), name='user_add_new')
+    path('carpark-detail/<str:carpark_id>', views.CarparkDetail.as_view()),
 ]
--- restful_apis_with_jwt/views.py
+++ restful_apis_with_jwt/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from car_park_data_handler.serializers import CarparkSerializer
-from car_park_data_handler.models import Carpark
+from car_park_data_handler.serializers import CarparkSerializer, CarparkDataSerializer
+from car_park_data_handler.models import Carpark, CarparkData
 from user_management.models import Account
 import json
 
@@ -27,19 +27,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-# APIs for frontend
-class CarparkList(APIView):
-    # this 1 line is all you need to protect API endpoint
-    # also, note that this authentication will apply to all methods in this class
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        carparks = Carpark.objects.all()
-        serializer = CarparkSerializer(carparks, many=True)
-
-        return Response(serializer.data)
-
-
 class UserAddNew(APIView):
     def post(self, request):
         requestBody = json.loads(request.body)
@@ -56,3 +43,28 @@ class UserAddNew(APIView):
 
         return Response(
             {"status": "success", "message": "New user successfully created.", "email": requestBody["email"]})
+
+
+# APIs for frontend
+class CarparkList(APIView):
+    # this 1 line is all you need to protect API endpoint
+    # also, note that this authentication will apply to all methods in this class
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        carparks = Carpark.objects.all()
+        serializer = CarparkSerializer(carparks, many=True)
+
+        return Response(serializer.data)
+
+
+class CarparkDetail(APIView):
+    """returns JSON with two keys, timestamp and available_lots, with value being an array for each"""
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, carpark_id):
+        data = CarparkData.objects.filter(carpark_id=carpark_id)
+        aggregated = {"timestamp": list(data.values_list('timestamp', flat=True)),
+                      "available_lots": list(data.values_list('available_lots', flat=True))}
+
+        return Response(aggregated)
