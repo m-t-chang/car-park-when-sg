8ce5603f2ead7ffb9096e7dbf8250dd29b89d241
mtchang <michaeltgchang@gmail.com>
2022-02-11 01:20:19 +0800

    CORS!!!!!!

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,7 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://car-park-when.netlify.app"
 ]
 
 LOGGING = {
