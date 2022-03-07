96676d0bbffa8123ba37b0ee3ad6a2be551d518e
mtchang <michaeltgchang@gmail.com>
2022-03-08 00:43:15 +0800

    delete old scraper and mongoDB files

--- car_park_data_handler/urls.py
+++ car_park_data_handler/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('scrape/', views.scrape, name='scrape'),
-    path('transform/', views.transform_to_sql, name='transform'),
     path('<str:car_park_id>/', views.detail, name='detail'),
 ]
--- scripts/run_scraper.py
+++
@@ -1,7 +0,0 @@
-"""pings the endpoint to run the data scraper"""
-import requests
-import os
-import dotenv
-
-dotenv.load_dotenv()
-requests.post(os.environ.get("SCRAPER_ENDPOINT"))
